1ba7bcea68f273ed02cf93ca20f71954cba9f0c5
Madina Bektemirova <madina7051995@gmail.com>
2020-02-24 22:14:01 +0100

    add poi component view

--- src/com/POIComponent.java
+++ src/com/POIComponent.java
@@ -1,6 +1,11 @@
 package com;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 public class POIComponent implements IComponent {
 
@@ -15,7 +20,26 @@ public class POIComponent implements IComponent {
     }
 
     private void initView() {
+        BorderPane mainPane = new BorderPane();
+        mainPane.setPadding(new Insets(20, 0, 20, 20));
         view = new Pane();
+
+        Button sendButton = new Button("Send");
+        VBox vBox = new VBox(8);
+
+        CheckBox checkBox = new CheckBox("Schools");
+        vBox.getChildren().add(checkBox);
+
+        checkBox = new CheckBox("Restaurants");
+        vBox.getChildren().add(checkBox);
+
+
+        checkBox = new CheckBox("Hospitals");
+        vBox.getChildren().add(checkBox);
+
+        mainPane.setCenter(vBox);
+        mainPane.setBottom(sendButton);
+        view = mainPane;
     }
 
     @Override
